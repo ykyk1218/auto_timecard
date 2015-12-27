cea5d517741f5b1112f77668da96c974731584fd
YoshikiKobayashi <attack_no22001@yahoo.co.jp>
2015-12-27 22:17:14 +0900

    Initial Commit

---
+++ auto_timecartTests/auto_timecartTests.swift
@@ -0,0 +1,36 @@
+//
+//  auto_timecartTests.swift
+//  auto_timecartTests
+//
+//  Created by 小林芳樹 on 2015/12/27.
+//  Copyright © 2015年 小林芳樹. All rights reserved.
+//
+
+import XCTest
+@testable import auto_timecart
+
+class auto_timecartTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
